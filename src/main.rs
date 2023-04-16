82f71ae873b15cf5b3529ea266e7b4a6de04846d
Tony Rizko <tony@rizkocircle.com>
2023-04-15 17:45:34 -0700

    Fix error logging and process exit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use lrs::CLI;
-use std::env;
+use std::{env, process};
 
 fn main() {
     match CLI::from_args(env::args()) {
         Ok(cli) => cli.run(),
-        Err(e) => eprintln!("{:?}", e.to_string()),
+        Err(e) => {
+            eprintln!("{}", e.to_string());
+            process::exit(1);
+        },
     }
 }
